91c7b77382bab5adcff3f0da958b8845a37350ee
SD91PL <sd91pl@gmail.com>
2024-08-24 11:03:40 +0200

    configured babel packages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import babel from 'vite-plugin-babel'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/ToDoList/"
+	plugins: [
+		react(
+			babel({
+				babelConfig: {
+					presets: [
+						[
+							'@babel/preset-env',
+							{
+								targets: '> 0.25%, not dead',
+							},
+						],
+						'@babel/preset-react',
+					],
+				},
+			})
+		),
+	],
+	build: {
+		target: 'es2015',
+	},
 })
