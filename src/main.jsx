69464029bee58e5f9a76a4fd85cf0e8645137bb5
SD91PL <sd91pl@gmail.com>
2024-11-17 08:14:44 +0100

    added animated intro - Intro.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Intro from './Intro.jsx'
 import './index.css'
 
+const Main = () => {
+	const [showIntro, setShowIntro] = useState(true)
+
+	return (
+		<>
+			{showIntro && <Intro onFinish={() => setShowIntro(false)} />}
+			<App />
+		</>
+	)
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Main />
+	</React.StrictMode>
 )
