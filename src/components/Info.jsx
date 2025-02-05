002f56a642a63144ed30cf85e4c1a368a3eeae34
SD91PL <sd91pl@gmail.com>
2025-02-05 22:13:20 +0100

    improved UI/UX - Info and InfoDnd

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -10,7 +10,8 @@ export default function Info({ highlight }) {
 	return (
 		<div className='container mt-6 px-1 text-center text-[#2d4b4d] text-sm sm:text-base'>
 			<p className='leading-loose'>
-				<span className={highlighted}>Write your first task</span> <br />
+				<strong className={highlighted}>Write your first task</strong>{' '}
+				<br className={`sm:hidden`} />
 				and press <strong translate='no'>Enter</strong> or click
 				<strong> Add</strong>
 			</p>
--- src/components/InfoDnd.jsx
+++ src/components/InfoDnd.jsx
@@ -1,8 +1,7 @@
 export default function InfoDnd() {
 	return (
-		<div className='container mt-3 px-1 text-center text-[#2d4b4d] text-sm sm:text-base'>
+		<div className='container mt-2 px-1 text-center text-[#2d4b4d] text-sm sm:text-base'>
 			<p className='leading-loose'>
-				<span className='hidden sm:inline'>Now, you can </span>
 				<strong className='capitalize'>Drag and drop </strong>
 				to reorder your tasks
 			</p>
